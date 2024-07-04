b640989ec87a6cc61dbce91195bd917cf3f68f02
damiancaso <125307407+damiancaso@users.noreply.github.com>
2024-07-03 19:57:50 -0500

    Update _dbEkekos_v01.cs

--- EKEKOSDATABASEMODEL/_dbEkekos_v01.cs
+++ EKEKOSDATABASEMODEL/_dbEkekos_v01.cs
@@ -61,7 +61,7 @@ public partial class _dbEkekos_v01 : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("data source=ekekov03.mssql.somee.com;persist security info=False;initial catalog=ekekov03;user id=JoseT_SQLLogin_1;pwd=nxusthytor;Trust Server Certificate=true");
+        => optionsBuilder.UseSqlServer("data source=ekekos_B5.mssql.somee.com;persist security info=False;initial catalog=ekekos_B5;user id=Deymian_SQLLogin_1;pwd=9riup1jf2h;Trust Server Certificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
